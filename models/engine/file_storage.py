ba4d35a1a286a0d9f3a6a501451db16019221590
Lolu01 <jaiyeolalulu@gmail.com>
2024-02-11 08:47:26 -0800

    Ghost

---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" """
+import json
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class FileStorage:
+    """
+    """
+    def __init__(self, path="file.json"):
+        """
+        """
+        self.__file_path = path
+        self.__objects = {}
+
+    def all(self):
+        """
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        """
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """
+        """
+        objs_dict = {k: v.to_dict() for k, v in self.__objects.items()}
+        json_str = json.dumps(objs_dict)
+        with open(self.__file_path, 'w') as f:
+            f.write(json_str)
+
+    def reload(self):
+        """
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                json_str = f.read()
+                objs_dict = json.loads(json_str)
+                self.__objects = {k: globals()[v['__class__']](**v)
+                                  for k, v in objs_dict.items()}
+        except FileNotFoundError:
+            pass
